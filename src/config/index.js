fbc30797609cf985519ebba3c57af208a60334dc
unknown <cherltonmhangwana@gmail.com>
2026-08-21 01:48:30 +0200

    updates frontend

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const backendHost = import.meta.env.VITE_BACKEND_HOST || 'http://localhost';
 const backendPort = import.meta.env.VITE_BACKEND_PORT || '5000';
-const defaultApiBaseUrl = `${backendHost}:${backendPort}`;
+const defaultApiBaseUrl = `https://mood-dj-backend-nhc0.onrender.com`;
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || defaultApiBaseUrl;
 
 export const config = {
